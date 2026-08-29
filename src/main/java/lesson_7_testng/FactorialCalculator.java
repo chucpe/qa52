41fdedd73abcee5686c8edbe62ccc1641d0842c9
qa52 <ie@ya.ru>
2026-08-30 02:04:14 +0300

    2.7 TESTNG

---
+++ src/main/java/lesson_7_testng/FactorialCalculator.java
@@ -0,0 +1,18 @@
+package lesson_7_testng;
+
+public class FactorialCalculator {
+
+    public static long calculateFactorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Number must be non-negative");
+        }
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
---
+++ src/main/java/lesson_7_testng/NumberComparator.java
@@ -0,0 +1,20 @@
+package lesson_7_testng;
+
+public class NumberComparator {
+
+    public static int compare(int a, int b) {
+        return Integer.compare(a, b);
+    }
+
+    public static boolean isGreater(int a, int b) {
+        return a > b;
+    }
+
+    public static boolean isLess(int a, int b) {
+        return a < b;
+    }
+
+    public static boolean isEqual(int a, int b) {
+        return a == b;
+    }
+}
